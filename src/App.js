caf99e7a94cf30e27af71b370c2dbad17b47c494
supminn <30731236+supminn@users.noreply.github.com>
2021-07-03 17:57:23 +0530

    refactor: products.json added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import { Filters } from "./components/filters/Filters";
 import { Navigation } from "./components/navigation/Navigation";
 import { ProductListing } from "./components/productListing/productListing";
-import { products } from "./database/products";
+// import { products } from "./database/products";
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
 
 function App() {
-  return (
+  let [products, setProducts] = useState(null);
+  let [showLoader, setShowLoader] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get("products.json");
+        setProducts(data.products);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setShowLoader(false);
+      }
+    })();
+  }, []);
+  return showLoader ? (
+    <div className="position-center">
+      <Loader type="Oval" color="#2874f0" height={80} width={80} />
+    </div>
+  ) : (
     <div className="layout">
       <div className="nav">
         <Navigation />
